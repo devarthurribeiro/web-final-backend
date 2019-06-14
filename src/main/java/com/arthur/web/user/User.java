7b12d3982bb2e11493376040335e3eacce0b2cf4
Arthur Ribeiro <devarthurribeiro@gmail.com>
2019-06-14 04:17:24 -0300

    Finalizado crud de usuário

--- src/main/java/com/arthur/web/user/User.java
+++ src/main/java/com/arthur/web/user/User.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @AllArgsConstructor
@@ -13,6 +15,7 @@ import javax.persistence.Id;
 @Entity(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
--- src/main/java/com/arthur/web/user/UserController.java
+++ src/main/java/com/arthur/web/user/UserController.java
@@ -1,10 +1,7 @@
 package com.arthur.web.user;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +25,31 @@ public class UserController {
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return repository.save(user);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity update(@PathVariable("id") long id,
+                                 @RequestBody User user) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(user.getName());
+                    record.setEmail(user.getEmail());
+                    User updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
 }
